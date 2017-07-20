acc347cfbba0a4690e8354fa076ea52752740009
Nicolas Dupont <nicolas@akeneo.com>
2017-07-20 10:18:33 +0200

    Fix ufo animation

--- src/world/ufo.ts
+++ src/world/ufo.ts
@@ -11,6 +11,9 @@ export class Alien extends Phaser.Sprite
         group.add(this);
 
         this.animations.add('idle', [0, 1, 2, 3, 4, 5, 6, 7], 4, true);
-        this.animations.add('killed', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], 8, true);
+        this.animations.add('killed', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24], 8, true);
+        this.animations.add('headshot', [30, 31, 32, 33, 34, 35, 36, 37, 38, 39], 8, true);
+        this.animations.add('falling', [50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63], 8, true);
+        this.animations.add('headshot2', [70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83], 8, true);
     }
 }
